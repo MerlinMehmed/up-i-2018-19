6ef189ef7248b2c76f3dfd68651feec6b714075a
Merlin Mehmed <merlinmehmed17@gmail.com>
2018-11-02 22:26:49 +0200

    Add week04 and week05 tasks solutions.

---
+++ Week04/solutions/Task4.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Не знаем колко точно числа ще въведе потребителят, 
+	// но знаем че ще бъдат най-много 100, 
+	// за това създаваме масив от 100 елемента.
+	int numbers[100];
+
+	// Въвеждане на брой числа от клавиатурата
+	// и проверка на условието броят на числата да бъде най-малко 1 и най-много 100.
+	int n;
+	do
+	{
+		cout << "Enter positive count of numbers: ";
+		cin >> n;
+	} while (n < 1 || n > 100);
+
+	// Въвеждане на n брой числа от клавиатурата
+	for (int i = 0; i < n; i++)
+	{
+		// Въвеждане на число в масива на индекс i
+		cout << "Enter number " << i + 1 << ": ";
+		cin >> numbers[i];
+	}
+
+	int m;
+	cout << "Enter number to search for: ";
+	cin >> m;
+
+	// Търсене на въведеното число в масива
+	int i = 0;
+	do
+	{
+		i++;
+	} while (i < n && numbers[i] != m);
+
+	if (numbers[i] == m) {
+		cout << "Found" << endl;
+	}
+	else {
+		cout << "Not found" << endl;;
+	}
+
+	return 0;
+}
---
+++ Week04/solutions/Task5.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Не знаем колко точно числа ще въведе потребителят, 
+	// но знаем че ще бъдат най-много 100, 
+	// за това създаваме масив от 100 елемента.
+	int numbers[100];
+
+	// Въвеждане на брой числа от клавиатурата
+	// и проверка на условието броят на числата да бъде най-малко 1 и най-много 100.
+	int n;
+	do
+	{
+		cout << "Enter positive count of numbers: ";
+		cin >> n;
+	} while (n < 1 || n > 100);
+
+	// Въвеждане на n брой числа от клавиатурата
+	for (int i = 0; i < n; i++)
+	{
+		// Въвеждане на число в масива на индекс i
+		cout << "Enter number " << i + 1 << ": ";
+		cin >> numbers[i];
+	}
+
+	int i, j;
+	do {
+		cout << "Enter indexes to swap: ";
+		cin >> i >> j;
+	} while (i < 0 || j < 0 || i >= n || j >= n);
+
+	// Размяна на стойностите
+	int temp = numbers[i];
+	numbers[i] = numbers[j];
+	numbers[j] = temp;
+
+	// Извеждане на масива след размяната
+	for (int k = 0; k < n; k++)
+	{
+		cout << numbers[k] << " ";
+	}
+
+	return 0;
+}
