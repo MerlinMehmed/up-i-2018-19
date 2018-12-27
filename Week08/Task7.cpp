eae3703d380571d422db2530b10bc17d42408f83
Merlin Mehmed <merlinmehmed17@gmail.com>
2018-12-27 13:15:12 +0200

    Task 7 from week 8 solution.

--- Week08/Task7.cpp
+++ Week08/Task7.cpp
@@ -2,18 +2,75 @@
 using namespace std;
 
 int main() {
-	char s1[100];
-	cin.getline(s1, 100);
-	char s2[100];
-	int j = 0;
-	for (size_t i = 0; i < strlen(s1); i++) {
-		if (s1[i] >= 'a' && s1[i] <= 'z') {
-			s2[j] = s1[i];
-			j++;
+
+	int matrix[100][100];
+	int n;
+	do {
+		cin >> n;
+	} while (n <= 0 || n > 100);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> *(*(matrix + i) + j);
 		}
 	}
-	s2[j] = '\0';
-	cout << s2;
+
+	bool magical = true;
+
+	// Search rows
+	int rowProduct = 1;
+	for (int i = 0; i < n; i++) {
+		if (!magical) {
+			break;
+		}
+		int curProduct = 1;
+		for (int j = 0; j < n; j++) {
+			curProduct *= *(*(matrix + i) + j);
+		}
+		if (i == 0)
+			rowProduct = curProduct;
+		else if (curProduct != rowProduct) {
+			cout << "Different";
+			magical = false;
+		}
+	}
+
+	// Search columns
+	int colProduct = 1;
+	for (int i = 0; i < n; i++) {
+		if (!magical) break;
+		int currentProduct = 1;
+		for (int j = 0; j < n; j++) {
+			currentProduct *= *(*(matrix + j) + i);
+		}
+		if (i == 0) colProduct = currentProduct;
+		else if (currentProduct != rowProduct) {
+			magical = false;
+		}
+	}
+
+	// Find product of first diagonal
+	int d1Product = 1;
+	for (int i = 0, j = 0; i < n, j < n; i++, j++) {
+		d1Product *= *(*(matrix + i) + j);
+	}
+
+	// Find product of second diagonal
+	int d2Product = 1;
+	for (int i = 0, j = n - 1; i < n, j >= 0; i++, j--) {
+		d2Product *= *(*(matrix + i) + j);
+	}
+
+	if (rowProduct && colProduct &&d1Product && d2Product)
+	{
+		magical = true;
+	}
+	else
+	{
+		magical = false;
+	}
+	
+	magical ? cout << "Yes" << endl : cout << "No" << endl;
 
 	system("pause");
 	return 0;
